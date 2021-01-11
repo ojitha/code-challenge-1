38364d51116d79aaad80853d95edb99c404da8cf
ojitha <ojithak@gmail.com>
2021-01-11 14:31:37 +1100

    created class for configuration

--- Second_step.py
+++ Second_step.py
@@ -5,6 +5,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import udf
+from utils import removeStartEndChars
+
+
+# created a UDF to cleanse for example
+cleanCol = udf(lambda x: removeStartEndChars(x), StringType())
 
 spark = SparkSession.builder.appName('abc').getOrCreate()
 
@@ -18,4 +23,10 @@ csv_df = df.select(
     df.value.substr(121,12).alias('dob')
 )
 csv_df.show()
-csv_df.write.format('csv').save('data/mytest.csv')
+df_to_save = csv_df \
+    .withColumn('first_name', cleanCol('first_name')) \
+    .withColumn('last_name', cleanCol('last_name')) \
+    .withColumn('address', cleanCol('address')) \
+    .withColumn('dob', cleanCol('dob'))
+
+df_to_save.write.option('delimiter','|').format('csv').save('data/mytest.csv')
--- utils.py
+++ utils.py
@@ -1,7 +1,50 @@
 import re
+import ConfigParser
 
+#constants
+WIDTH ='width'
+NAME = 'name'
 #cleansing the pre and post \' characters
 def removeStartEndChars(str):
     pre = re.sub("^\'","",str)
     post = pre[:-1]
     return post
+
+# file confituration from config.ini files is stored here for resuablility
+class FileConfig:
+
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read('config.ini')
+
+        # first_name
+        self.first_name = {NAME:config.get('FIXED_WIDTH_FILE','first_name.name')
+            ,WIDTH:int(config.get('FIXED_WIDTH_FILE','first_name.width'))}
+        # last_name
+        self.last_name = {NAME:config.get('FIXED_WIDTH_FILE','last_name.name')
+            ,WIDTH:int(config.get('FIXED_WIDTH_FILE','last_name.width'))}
+
+        # address
+        self.address = {NAME:config.get('FIXED_WIDTH_FILE','address.name')
+            ,WIDTH:int(config.get('FIXED_WIDTH_FILE','address.width'))}
+
+        # dob
+        self.dob = {NAME:config.get('FIXED_WIDTH_FILE','dob.name')
+            ,WIDTH:int(config.get('FIXED_WIDTH_FILE','dob.width'))}
+    
+
+    @property
+    def first_name(self):
+        return self.first_name
+
+    @property
+    def last_name(self):
+        return self.last_name
+
+    @property
+    def address(self):
+        return self.address
+
+    @property
+    def dob(self):
+        return self.dob
