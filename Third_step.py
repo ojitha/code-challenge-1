692383e069f1224ca8fcc3af1639c97c5d46c554
ojitha <ojithak@gmail.com>
2021-01-11 15:52:00 +1100

    add the configuration to third step

--- Third_step.py
+++ Third_step.py
@@ -2,19 +2,37 @@ from pyspark.sql import SparkSession
 from faker import Factory
 from pyspark.sql.functions import udf
 from pyspark.sql.types import *
+import ConfigParser
+#import logging
 
+# # logging.basicConfig(filename='today.log',level=logging.INFO)
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
+
+#create faker factory
 fake = Factory.create('en_AU')
 
+#user define function for the first name faker
 def fake_first_name():
     return fake.first_name()
+fake_first_name_udf = udf(fake_first_name, StringType())
 
+#user define function for the last name faker
+def fake_last_name():
+    return fake.last_name()
+fake_last_name_udf = udf(fake_last_name, StringType())
+
+#user define function for the address faker
+def fake_address():
+    return fake.address().replace('\n','')
+fake_address_udf = udf(fake_address, StringType())
 
 spark = SparkSession.builder.appName('Third_step').getOrCreate()
-df = spark.read.option('header',True).option('delimiter','|').option("inferSchema",True).csv("data/cvs_file.csv")
-print('----------')
-df.printSchema()
+df = spark.read.option('header',True).option('delimiter','|').option("inferSchema",True).csv("data/"+config.get('DEFAULT','csv_file.name'))
+#logging.info(df.printSchema())
+
 df.show(truncate=False)
 
-fake_first_name_udf = udf(fake_first_name, StringType())
-df1 = df.withColumn('first_name', fake_first_name_udf())
+
+df1 = df.withColumn('first_name', fake_first_name_udf()).withColumn('last_name', fake_last_name_udf()).withColumn('address', fake_address_udf())
 df1.show(truncate=False)
