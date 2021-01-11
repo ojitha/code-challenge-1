6a98fb4f1266f8b1e586c722aea4205ca2f4ee70
ojitha <ojithak@gmail.com>
2021-01-11 14:42:52 +1100

    updated test for the FileConfig

--- Test_utils.py
+++ Test_utils.py
@@ -1,10 +1,28 @@
 import unittest
-from utils import removeStartEndChars
+from utils import removeStartEndChars, FileConfig, NAME, WIDTH
 import logging
+import ConfigParser
 
 class Cleansing(unittest.TestCase):
-    # def setUp(self):
-    #     logging.basicConfig(filename='test.log', level=logging.DEBUG)
+    def setUp(self):
+        logging.basicConfig(filename='test.log', level=logging.DEBUG)
+        self.config = ConfigParser.ConfigParser()
+        self.config.read('config.ini')
 
     def test_removeStartEndChars(self):
-        print(removeStartEndChars("'Ojitha'"))
+        self.assertEquals(removeStartEndChars("'Ojitha'"),'Ojitha')
+
+
+    def test_FileConfig(self):
+        f_config = FileConfig()
+        self.assertEquals(f_config.first_name[NAME], self.config.get('FIXED_WIDTH_FILE','first_name.name') )
+        self.assertEquals(f_config.first_name[WIDTH], int(self.config.get('FIXED_WIDTH_FILE','first_name.width')) )
+
+        self.assertEquals(f_config.last_name[NAME], self.config.get('FIXED_WIDTH_FILE','last_name.name') )
+        self.assertEquals(f_config.last_name[WIDTH], int(self.config.get('FIXED_WIDTH_FILE','last_name.width')) )
+
+        self.assertEquals(f_config.address[NAME], self.config.get('FIXED_WIDTH_FILE','address.name') )
+        self.assertEquals(f_config.address[WIDTH], int(self.config.get('FIXED_WIDTH_FILE','address.width')) )
+
+        self.assertEquals(f_config.dob[NAME], self.config.get('FIXED_WIDTH_FILE','dob.name') )
+        self.assertEquals(f_config.dob[WIDTH], int(self.config.get('FIXED_WIDTH_FILE','dob.width')) ) 
